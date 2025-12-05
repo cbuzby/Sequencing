1b98aa409fbcceefc0eac77961bdf7bb4cf1a3da
Cassandra <cassandra.buzby@gmail.com>
2025-12-05 16:51:48 -0500

    Fall 2025
    Adding everything since the start of the semester

--- Analysis/Summer2025/SinglePoolProcess.R
+++ Analysis/Summer2025/SinglePoolProcess.R
@@ -1,6 +1,5 @@
 #!/usr/bin/env Rscript
 
-
 # Install Packages if Needed ----------------------------------------------
 
 # Helper function to install CRAN packages if missing
@@ -22,6 +21,11 @@ install_github_if_missing <- function(pkg, repo) {
 
 # Install CRAN package
 install_if_missing("tidyr")
+install_if_missing("reshape2")
+install_if_missing("data.table")
+install_if_missing("doParallel")
+install_if_missing("scales")
+install_if_missing("stringr")
 
 # Install GitHub package (replace with actual repo)
 install_github_if_missing("cybrBSA", "cbuzby/cybrBSA")
@@ -30,7 +34,11 @@ install_github_if_missing("cybrBSA", "cbuzby/cybrBSA")
 
 require(tidyr)
 require(cybrBSA)
-
+require(reshape2)
+require(data.table)
+require(doParallel)
+require(scales)
+require(stringr)
 
 # Load in Data from command line  ------------------------------------------------------------
 
@@ -38,20 +46,20 @@ myargs <- commandArgs(trailingOnly = TRUE)
 
 myfile <- myargs[1]
 myfilename <- myargs[2]
-myMQCfile <- myargs[3]
+#myMQCfile <- myargs[3] #don't actually need for this section
 
-MQC <- read.csv(myMQCfile)
+#MQC <- read.csv(myMQCfile)
 
 # Load in Data in R ------------------------------------------------------------
 #myfile <- "C://Users//cassa//Documents//SiegalLab//Data//Jan25_CSS15.SortedCat.vcf.output.table" #ChangeThis
 #MQC <- read.csv("C://Users//cassa//Documents//SiegalLab//Subsampling//MQC_Annotated_REVISED.csv")
 
-MQC %>% select(Pool, PoolID, Dataset = Library, Parent, Bulk, Rep) -> MQC_Key
+#MQC %>% dplyr::select(Pool, PoolID, Dataset = Library, Parent, Bulk, Rep) -> MQC_Key
 
 
 #GET RID OF EXCLUDED ONES
 read.table(myfile, header = TRUE) -> testrawdata
-testrawdata %>% select(CHROM, POS, ALT) %>% separate(ALT, into = c("First", "Second"), sep = ",") %>% 
+testrawdata %>% dplyr::select(CHROM, POS, ALT) %>% separate(ALT, into = c("First", "Second"), sep = ",") %>% 
   na.omit() -> exclude
 
 ChromKey <- data.frame(chromosomes = c("I", "II", "III", "IV", "V", "VI", "VII", "VIII",
@@ -61,25 +69,25 @@ ChromKey <- data.frame(chromosomes = c("I", "II", "III", "IV", "V", "VI", "VII",
                                  "NC_001141.2", "NC_001142.9", "NC_001143.9", "NC_001144.5",
                                  "NC_001145.3", "NC_001146.8", "NC_001147.6", "NC_001148.4", "NC_001224.1"))
 
-exclude %>% left_join(.,ChromKey) %>% select(-CHROM) %>% mutate(CHROM = chromosomes) %>% 
-  select(-chromosomes) -> exclude
+exclude %>% dplyr::left_join(.,ChromKey) %>% dplyr::select(-CHROM) %>% dplyr::mutate(CHROM = chromosomes) %>% 
+  dplyr::select(-chromosomes) -> exclude
 
-exclude %>% mutate(locus = paste(CHROM, POS)) -> exclKEY
+exclude %>% dplyr::mutate(locus = paste(CHROM, POS)) -> exclKEY
 
-cybrInputGATKTable2(myfile) %>% mutate(Coverage = as.numeric(AD.REF) + as.numeric(AD.ALT)) %>%
-  select(POS, CHROM, Dataset, GQ, AD.REF, AD.ALT, Coverage) -> rawdata
+cybrInputGATKTable2(myfile) %>% dplyr::mutate(Coverage = as.numeric(AD.REF) + as.numeric(AD.ALT)) %>%
+  dplyr::select(POS, CHROM, Dataset, GQ, AD.REF, AD.ALT, Coverage) -> rawdata
 
 parentSNPids <- cybrConvertParentalAlleles(Truncate = TRUE)
 
-rawdata %>% merge(parentSNPids) %>% mutate(REFW = as.numeric(Type == "Wine"), REFO = as.numeric(Type == "Oak")) %>%
+rawdata %>% merge(parentSNPids) %>% dplyr::mutate(REFW = as.numeric(Type == "Wine"), REFO = as.numeric(Type == "Oak")) %>%
   group_by(Dataset, CHROM, POS) %>%
-  mutate(Wine = max(REFW * as.numeric(AD.REF), REFO * as.numeric(AD.ALT)),
+  dplyr::mutate(Wine = max(REFW * as.numeric(AD.REF), REFO * as.numeric(AD.ALT)),
          Oak = max(REFW * as.numeric(AD.ALT), REFO * as.numeric(AD.REF))) %>%
-  select(Dataset, POS, CHROM, Coverage, Wine, Oak) %>%
+  dplyr::select(Dataset, POS, CHROM, Coverage, Wine, Oak) %>%
   pivot_longer(c(Oak, Wine), names_to = "Allele", values_to = "Reads") -> rawdata_called
 
-rawdata_called %>% mutate(locus = paste(CHROM, POS)) %>% filter(locus %in% exclKEY$locus == FALSE) %>% 
-  select(-locus) -> rawdata_called_ex
+rawdata_called %>% dplyr::mutate(locus = paste(CHROM, POS)) %>% filter(locus %in% exclKEY$locus == FALSE) %>% 
+  dplyr::select(-locus) -> rawdata_called_ex
 
 #saveRDS(rawdata_called_ex, file = "CSS15_called_ex.rds")
 finalname_ex <- paste(myfilename, "_called_ex.rds", sep = "")
@@ -89,11 +97,11 @@ saveRDS(rawdata_called_ex, file = finalname_ex)
 
 rawdata_called_ex %>% ungroup() %>% group_by(Dataset) %>% summarize(AvgCovg = mean(Coverage, na.rm = TRUE)) -> Means
 
-rawdata_called_ex %>% left_join(Means) %>% ungroup() %>% na.omit() %>%
+rawdata_called_ex %>% dplyr::left_join(Means) %>% ungroup() %>% na.omit() %>%
   group_by(Dataset, CHROM, POS) %>% 
-  select(Dataset, CHROM, POS, Coverage, AvgCovg) %>%
+  dplyr::select(Dataset, CHROM, POS, Coverage, AvgCovg) %>%
   distinct() %>%
-  mutate(CovChange = log(Coverage/AvgCovg)) %>%
+  dplyr::mutate(CovChange = log(Coverage/AvgCovg)) %>%
   filter(CovChange > -1) -> rawdata_called_ex_Unsmoothed_cf
 
 rawdata_called_ex %>% merge(rawdata_called_ex_Unsmoothed_cf) %>%
